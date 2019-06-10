399585cc81eb64afac633b35a27aba892feb17a7
xuqingqi <xuqingqi01@gmail.com>
2019-06-10 11:24:41 +0800

    resolve problems

---
+++ src/leetcode/problems/LeetCode_0145_BinaryTreePostorderTraversal.java
@@ -0,0 +1,28 @@
+package leetcode.problems;
+
+import leetcode.shared.TreeNode;
+
+import java.util.List;
+
+/**
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ *
+ * Example:
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [3,2,1]
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+public class LeetCode_0145_BinaryTreePostorderTraversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return null;
+    }
+
+}
