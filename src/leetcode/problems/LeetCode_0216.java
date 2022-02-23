c35eccb6048f9f3efee4f1085232adc2c41885e4
qingqixu <qingqixu@tencent.com>
2022-02-23 15:12:41 +0800

    resolve 0216

--- src/leetcode/problems/LeetCode_0216.java
+++ src/leetcode/problems/LeetCode_0216.java
@@ -1,5 +1,8 @@
 package leetcode.problems;
 
+import leetcode.utils.DebugUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,7 +10,8 @@ import java.util.List;
  *
  * 216. Combination Sum III
  *
- * Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+ * Find all possible combinations of k numbers that add up to a number n,
+ * given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
  *
  * Note:
  *
@@ -24,8 +28,43 @@ import java.util.List;
  */
 public class LeetCode_0216 {
 
+    public static void main(String[] args) {
+        DebugUtils.print(
+                new LeetCode_0216().combinationSum3(9, 45)
+        );
+    }
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-        return null;
+        if (n <= 0 || n > 45) {
+            return new ArrayList<>();
+        }
+        return combinationSum3(
+                new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                0,
+                k,
+                n
+        );
+    }
+
+    public List<List<Integer>> combinationSum3(int[] nums, int start, int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (n == 0 && k == 0) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+
+        if (start >= nums.length || n < 0 || nums[start] > n) {
+            return result;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            List<List<Integer>> lists = combinationSum3(nums, i + 1, k - 1, n - nums[i]);
+            for (List<Integer> list : lists) {
+                list.add(0, nums[i]);
+                result.add(list);
+            }
+        }
+        return result;
     }
 
 }
