f5bdd19a0320e503ffb60efc9b0e19930b3c62ed
qingqixu <qingqixu@tencent.com>
2022-02-23 16:03:12 +0800

    resolve 0217

--- src/leetcode/problems/LeetCode_0217.java
+++ src/leetcode/problems/LeetCode_0217.java
@@ -1,5 +1,10 @@
 package leetcode.problems;
 
+import leetcode.utils.DebugUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/contains-duplicate/
  *
@@ -24,8 +29,62 @@ package leetcode.problems;
  */
 public class LeetCode_0217 {
 
+    public static void main(String[] args) {
+        DebugUtils.print(
+                new LeetCode_0217().containsDuplicate(
+                        new int[]{
+                                1,2,3,1
+                        }
+                )
+        );
+        DebugUtils.print(
+                new LeetCode_0217().containsDuplicate(
+                        new int[]{
+                                1,2,3,4
+                        }
+                )
+        );
+        DebugUtils.print(
+                new LeetCode_0217().containsDuplicate(
+                        new int[]{
+                                1,1,1,3,3,4,3,2,4,2
+                        }
+                )
+        );
+
+    }
+
     public boolean containsDuplicate(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            }
+            map.put(num, null);
+        }
         return false;
     }
+
+    public boolean containsDuplicate2(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            }
+            map.put(num, null);
+        }
+        return false;
+    }
+
+    // quick select
+
 
 }
