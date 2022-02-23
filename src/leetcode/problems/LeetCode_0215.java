9a09e3219a7149441a8502d2f6b92b58f23fdc67
qingqixu <qingqixu@tencent.com>
2022-02-23 14:42:32 +0800

    resolve 0215

--- src/leetcode/problems/LeetCode_0215.java
+++ src/leetcode/problems/LeetCode_0215.java
@@ -1,5 +1,7 @@
 package leetcode.problems;
 
+import leetcode.utils.DebugUtils;
+
 /**
  * https://leetcode.com/problems/kth-largest-element-in-an-array/
  *
@@ -20,8 +22,74 @@ package leetcode.problems;
  */
 public class LeetCode_0215 {
 
+    public static void main(String[] args) {
+        DebugUtils.print(
+                new LeetCode_0215().findKthLargest(
+                        new int[]{
+                                3,2,3,1,2,4,5,5,6
+                        },
+                        4
+                )
+        );
+    }
+
     public int findKthLargest(int[] nums, int k) {
-        return 0;
+        quickSort(nums, 0, nums.length - 1);
+        return nums[k - 1];
+    }
+
+    private void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int key = nums[start];
+        int lo = start;
+        int hi = end;
+        while (lo <= hi) {
+            if (nums[lo] >= key) {
+                lo++;
+                continue;
+            }
+            swap(nums, lo, hi);
+            hi--;
+        }
+        swap(nums, start, hi);
+        quickSort(nums, start, hi - 1);
+        quickSort(nums, hi + 1, end);
+    }
+
+    private void swap(int[] nums, int from, int to) {
+        if (from == to) {
+            return;
+        }
+        nums[from] ^= nums[to];
+        nums[to] ^= nums[from];
+        nums[from] ^= nums[to];
+    }
+
+    // TODO
+    int quickSelect(int[] nums, int low, int high, int k) {
+        int pivot = low;
+
+        // use quick sort's idea
+        // put nums that are <= pivot to the left
+        // put nums that are  > pivot to the right
+        for (int j = low; j < high; j++) {
+            if (nums[j] <= nums[high]) {
+                swap(nums, pivot++, j);
+            }
+        }
+        swap(nums, pivot, high);
+
+        // count the nums that are > pivot from high
+        int count = high - pivot + 1;
+        // pivot is the one!
+        if (count == k) return nums[pivot];
+        // pivot is too small, so it must be on the right
+        if (count > k) return quickSelect(nums, pivot + 1, high, k);
+        // pivot is too big, so it must be on the left
+        return quickSelect(nums, low, pivot - 1, k - count);
     }
 
 }
