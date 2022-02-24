fed57ab9e0ff42d83064e26d9aa2a6dedc92a457
qingqixu <qingqixu@tencent.com>
2022-02-24 19:16:41 +0800

    resolve 0221

--- src/leetcode/problems/LeetCode_0221.java
+++ src/leetcode/problems/LeetCode_0221.java
@@ -36,16 +36,21 @@ public class LeetCode_0221 {
     }
 
     public int maximalSquare(char[][] matrix) {
+        return solution2(matrix);
+    }
+
+    // for every
+    public int solution1(char[][] matrix) {
         int maxLength = 0;
         for (int column = 0; column < matrix.length; column++) {
             for (int row = 0; row < matrix[column].length; row++) {
-                maxLength = Math.max(maxLength, maximalSquare(matrix, column, row));
+                maxLength = Math.max(maxLength, solution1(matrix, column, row));
             }
         }
         return maxLength * maxLength;
     }
 
-    public int maximalSquare(char[][] matrix, int column, int row) {
+    public int solution1(char[][] matrix, int column, int row) {
         if (matrix[column][row] == '0') {
             return 0;
         }
@@ -68,6 +73,61 @@ public class LeetCode_0221 {
         }
 
         return length;
+    }
+
+    // dp
+    public int solution2(char[][] matrix) {
+        int[][] table = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = -1;
+            }
+        }
+
+        if (matrix[0][0] == '0') {
+            table[0][0] = 0;
+        } else {
+            table[0][0] = 1;
+        }
+
+        int maxLength = 0;
+        for (int column = 0; column < matrix.length; column++) {
+            for (int row = 0; row < matrix[column].length; row++) {
+                maxLength = Math.max(maxLength, solution2(matrix, column, row, table));
+            }
+        }
+        return maxLength * maxLength;
+    }
+
+    public int solution2(char[][] matrix, int column, int row, int[][] table) {
+        if (column < 0 || row < 0) {
+            return 0;
+        }
+
+        if (table[column][row] >= 0) {
+            return table[column][row];
+        }
+
+        if (matrix[column][row] == '0') {
+            table[column][row] = 0;
+            return table[column][row];
+        }
+
+        if (column == 0 || row == 0) {
+            table[column][row] = 1;
+            return table[column][row];
+        }
+
+        int lengthDiagonal = solution2(matrix, column - 1, row - 1, table);
+        if (lengthDiagonal == 0) {
+            table[column][row] = 1;
+            return table[column][row];
+        }
+
+        int lengthTop = solution2(matrix, column, row - 1, table);
+        int lengthLeft = solution2(matrix, column - 1, row, table);
+        table[column][row] = Math.min(Math.min(lengthLeft, lengthTop), lengthDiagonal) + 1;
+        return table[column][row];
     }
 
 
